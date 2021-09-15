2f682a1a7dc4ebc2189251e948276cde409e0c95
Ver33d <sergei33dd@mail.ru>
2021-09-15 17:38:01 +0300

    Update hash.h
    Refactoring

--- hash.h
+++ hash.h
@@ -7,12 +7,12 @@ using namespace std;
 class Hash
 {
     int CELL;		  
-    list<int> *table;		 // Указатель на массив, содержащий сегменты
+    list<int> *table;		 				// Указатель на массив, содержащий сегменты
     list<string> *word;	
 public:
-    Hash(int V);  					// Конструктор
-    void insertItem(string sumbol, int key);     // вставляем ключ в хеш-таблицу  
-    int hashFunction(int x) {   // хеш-функция для сопоставления значений с ключом
+    Hash(int V);  							// Конструктор
+    void insertItem(string sumbol, int key);// вставляем ключ в хеш-таблицу  
+    int hashFunction(int x) {   			// хеш-функция для сопоставления значений с ключом
         return (x % CELL);	 
     }
     void search(string _word);
